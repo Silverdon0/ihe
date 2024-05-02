ff48b618e79a1cbb96cf4442eeed8c01305f8c21
ugochukwu <Silverdonmentor@gmail.com>
2024-05-02 22:30:10 +0100

    some changes were made

---
+++ src/App.vue
@@ -0,0 +1,266 @@
+<template class="bg-gray-50">
+  <div >
+
+    <div>
+      <nav class="mt-3">
+        <span class="ml-40 pb-2 p-0.5 space-x-0.5 items-center border border-gray-200 rounded-full w-80  py-1 px-2 ">
+          <a href="#" class="text-blue-600 text-lg py-1 px-2">preline</a>
+          <a class="py-1 px-2 inline-flex items-center gap-x-3 text-white text-xs font-medium rounded-full bg-gray-800 hover:bg-gray-900 hover:text-white"
+            href="#"> Open Source </a>
+
+          <a class="py-1 px-2 inline-flex items-center gap-x-3 text-gray-800 text-xs font-medium rounded-full hover:text-gray-600    "
+            href="#">
+            Preline Pro
+            <span class="text-xs text-green-500 relative -ms-1">
+              <span class="flex absolute top-[5px] size-1.5">
+                <span class="animate-ping absolute inline-flex size-full rounded-full bg-green-400 opacity-75  "></span>
+                <span class="relative inline-flex rounded-full size-1.5 bg-green-500"></span>
+              </span>
+
+              <span class="ps-2.5">Live</span>
+            </span>
+          </a>
+        </span>
+
+        <span class="mt-10 ml-44">
+          <a href="#" class="text-gray-800  p-2 text-sm">Home</a>
+          <a href="#" class="text-gray-800  p-2 text-sm">Documentation</a>
+          <a class="inline-flex items-center text-blue-600 p-2 text-sm font-medium rounded-lg hover:text-blue-600 focus:outline-none focus:text-blue-600  active-link "
+            href="#">Examples</a>
+          <a href="#" class="text-gray-800  p-2 text-sm">Templates</a>
+          <a href="#"
+            class="ms-1 inline bg-blue-50 border border-blue-300 text-blue-600 text-[.6125rem] leading-4 uppercase align-middle rounded-full py-0.5 px-2 ">New</a>
+          <a href="#" class="text-gray-800  p-2 text-sm">Plugins</a>
+          <a href="#" class="text-gray-800  p-2 text-sm">Figma</a>
+ </span>
+
+ <div class="flex items-center lg:gap-x-1 ms-auto lg:order-3 float-right mr-24 mt-0.5">
+        
+        <button type="button" class="" aria-label="Search"><span class=""><svg width="20" height="20" class=""
+              viewBox="0 0 20 20" aria-hidden="true">
+              <path
+                d="M14.386 14.386l4.0877 4.0877-4.0877-4.0877c-2.9418 2.9419-7.7115 2.9419-10.6533 0-2.9419-2.9418-2.9419-7.7115 0-10.6533 2.9418-2.9419 7.7115-2.9419 10.6533 0 2.9419 2.9418 2.9419 7.7115 0 10.6533z"
+                stroke="currentColor" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round">
+              </path>
+            </svg></span></button>
+      
+
+        <div
+          class="flex items-center relative z-10 before:hidden lg:before:block before:w-px before:h-4 before:bg-gray-300  ">
+          <div class="lg:ms-1 flex">
+            <a class="inline-flex flex-shrink-0 justify-center items-center size-9 font-medium rounded-full text-gray-800 hover:bg-gray-200 "
+              href="#" target="_blank">
+              <svg class="flex-shrink-0 size-4" width="19" height="18" viewBox="0 0 19 18" fill="none"
+                xmlns="http://www.w3.org/2000/svg">
+                <path
+                  d="M6.875 18C8.531 18 9.875 16.656 9.875 15V12H6.875C5.219 12 3.875 13.344 3.875 15C3.875 16.656 5.219 18 6.875 18Z"
+                  fill="#0ACF83"></path>
+                <path d="M3.875 9C3.875 7.344 5.219 6 6.875 6H9.875V12H6.875C5.219 12 3.875 10.656 3.875 9Z"
+                  fill="#A259FF"></path>
+                <path d="M3.875 3C3.875 1.344 5.219 0 6.875 0H9.875V6H6.875C5.219 6 3.875 4.656 3.875 3Z"
+                  fill="#F24E1E"></path>
+                <path d="M9.87501 0H12.875C14.531 0 15.875 1.344 15.875 3C15.875 4.656 14.531 6 12.875 6H9.87501V0Z"
+                  fill="#FF7262"></path>
+                <path
+                  d="M15.875 9C15.875 10.656 14.531 12 12.875 12C11.219 12 9.87501 10.656 9.87501 9C9.87501 7.344 11.219 6 12.875 6C14.531 6 15.875 7.344 15.875 9Z"
+                  fill="#1ABCFE"></path>
+              </svg>
+            </a>
+            <a class="inline-flex flex-shrink-0 justify-center items-center size-9 font-medium rounded-full text-gray-800 hover:bg-gray-200    "
+              href="#" target="_blank">
+              <svg class="flex-shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                fill="currentColor" viewBox="0 0 16 16">
+                <path
+                  d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z">
+                </path>
+              </svg>
+            </a>
+            <a class="inline-flex flex-shrink-0 justify-center items-center size-9 font-medium rounded-full text-gray-800 hover:bg-gray-200    "
+              href="#" target="_blank">
+              <svg class="flex-shrink-0 size-3.5" width="48" height="50" viewBox="0 0 48 50" fill="none"
+                xmlns="http://www.w3.org/2000/svg">
+                <path
+                  d="M28.5665 20.7714L46.4356 0H42.2012L26.6855 18.0355L14.2931 0H0L18.7397 27.2728L0 49.0548H4.23464L20.6196 30.0087L33.7069 49.0548H48L28.5655 20.7714H28.5665ZM22.7666 27.5131L5.76044 3.18778H12.2646L42.2032 46.012H35.699L22.7666 27.5142V27.5131Z"
+                  fill="currentColor"></path>
+              </svg>
+            </a>
+          </div>
+
+
+          <span class="group inline-flex flex-shrink-0 justify-center items-center size-9 font-medium rounded-full text-gray-800 hover:bg-gray-200 dark:text-neutral-200 dark:hover:bg-neutral-800">
+              <svg class="flex-shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                <path d="M12 3a6 6 0 0 0 9 9 9 9 0 1 1-9-9Z"></path>
+              </svg>
+            </span>
+          
+        </div>
+
+        
+      </div>
+     </nav>
+    </div>
+
+    <div>
+      <div class="max-w-3xl mx-auto relative text-center px-4 sm:px-6 lg:px-8 py-10 md:py-24">
+        <div class="inline-block bg-gradient-to-tl from-blue-600 via-transparent to-purple-400 p-px rounded-xl mb-3">
+          <div class="bg-white rounded-xl py-1.5 px-3 text-3xl font-bold md:text-4xl lg:text-5xl lg:leading-tight ">
+            <span class="bg-clip-text bg-gradient-to-tl from-blue-600 to-purple-400 text-transparent">
+              185+
+            </span>
+          </div>
+        </div>
+
+        <h1 class="text-3xl font-bold md:text-4xl lg:text-5xl lg:leading-tight  ">Starter Pages &amp;
+          Examples</h1>
+        <p class="mt-4 md:text-lg text-gray-600  ">Kick-start your project effortlessly with Preline
+          UI's wide range of examples, using pre-built UI parts, custom components and layouts using Tailwind CSS.</p>
+      </div>
+    </div>
+
+
+    <div class="max-w-[85rem] mx-auto pb-12 px-4 sm:px-6 lg:px-8 divide-y divide-gray-200  ">
+      <div class="pt-10 lg:pt-10">
+        <div class="mb-10 lg:mb-14">
+          <h2 id="marketing" class="scroll-mt-12 mb-2">
+            <a class="text-blue-600 hover:text-blue-500 text-2xl font-semibold " href="#">
+              Marketing
+            </a>
+          </h2>
+
+          <p class="text-sm text-gray-600  ">
+            Premium grade comprehensive components and website solutions to create stunning marketing websites
+          </p>
+        </div>
+      </div>
+
+      <div class="py-10 lg:py-14">
+        <div id="marketing-page-sections" class="scroll-mt-12 mb-5">
+          <a class="text-gray-800 hover:text-gray-600 text-lg font-semibold    " href="#">
+            Page Sections
+          </a>
+        </div>
+
+      
+        <div class="text-start grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 xl:gap-6">
+         
+          <a class="relative rounded-xl before:absolute before:inset-0 before:z-10 before:border before:border-gray-200 before:rounded-xl before:transition before:hover:border-2 before:hover:border-blue-600 before:hover:shadow-lg      "
+            href="#">
+            <div class="relative pt-[50%]">
+              <img class="size-full absolute top-0 start-0 object-cover rounded-t-xl "
+                src="https://preline.co/assets/thumbnails/examples-dark/hero-sections.webp" alt="Hero Sections">
+
+            </div>
+            <div class="bg-white py-3.5 px-4 rounded-b-xl  ">
+              <h3 class="text-sm text-gray-900 font-medium  ">Hero Sections <span
+                  class="inline bg-blue-50 border border-blue-300 text-blue-600 text-[.6125rem] leading-4 uppercase align-middle rounded-full py-0.5 px-2 ">+2
+                  new</span></h3>
+              <p class="mt-0.5 text-sm text-gray-400  ">10 components</p>
+            </div>
+          </a>
+
+         
+          <a class="relative rounded-xl before:absolute before:inset-0 before:z-10 before:border before:border-gray-200 before:rounded-xl before:transition before:hover:border-2 before:hover:border-blue-600 before:hover:shadow-lg      "
+            href="#">
+            <div class="relative pt-[50%]">
+              <img class="size-full absolute top-0 start-0 object-cover rounded-t-xl "
+                src="https://preline.co/assets/thumbnails/examples/hero-forms.webp" alt="Hero Forms">
+
+            </div>
+            <div class="bg-white py-3.5 px-4 rounded-b-xl  ">
+              <h3 class="text-sm text-gray-900 font-medium  ">Hero Forms </h3>
+              <p class="mt-0.5 text-sm text-gray-400  ">4 components</p>
+            </div>
+          </a>
+         
+          <a class="relative rounded-xl before:absolute before:inset-0 before:z-10 before:border before:border-gray-200 before:rounded-xl before:transition before:hover:border-2 before:hover:border-blue-600 before:hover:shadow-lg      "
+            href="#">
+            <div class="relative pt-[50%]">
+              <img class="size-full absolute top-0 start-0 object-cover rounded-t-xl "
+                src="https://preline.co/assets/thumbnails/examples/icon-sections.webp" alt="Icon Sections">
+
+            </div>
+            <div class="bg-white py-3.5 px-4 rounded-b-xl  ">
+              <h3 class="text-sm text-gray-900 font-medium  ">Icon Sections </h3>
+              <p class="mt-0.5 text-sm text-gray-400  ">10 components</p>
+            </div>
+          </a>
+          
+          <a class="relative rounded-xl before:absolute before:inset-0 before:z-10 before:border before:border-gray-200 before:rounded-xl before:transition before:hover:border-2 before:hover:border-blue-600 before:hover:shadow-lg      "
+            href="#">
+            <div class="relative pt-[50%]">
+              <img class="size-full absolute top-0 start-0 object-cover rounded-t-xl "
+                src="https://preline.co/assets/thumbnails/examples/pricing-sections.webp" alt="Pricing Sections">
+
+            </div>
+            <div class="bg-white py-3.5 px-4 rounded-b-xl  ">
+              <h3 class="text-sm text-gray-900 font-medium  ">Pricing Sections </h3>
+              <p class="mt-0.5 text-sm text-gray-400  ">4 components</p>
+            </div>
+          </a>
+         
+          <a class="relative rounded-xl before:absolute before:inset-0 before:z-10 before:border before:border-gray-200 before:rounded-xl before:transition before:hover:border-2 before:hover:border-blue-600 before:hover:shadow-lg      "
+            href="#">
+            <div class="relative pt-[50%]">
+              <img class="size-full absolute top-0 start-0 object-cover rounded-t-xl"
+                src="https://preline.co/assets/thumbnails/examples/faq-sections.webp" alt="FAQ Sections">
+
+            </div>
+            <div class="bg-white py-3.5 px-4 rounded-b-xl ">
+              <h3 class="text-sm text-gray-900 font-medium  ">FAQ Sections </h3>
+              <p class="mt-0.5 text-sm text-gray-400  ">4 components</p>
+            </div>
+          </a>
+          
+          <a class="relative rounded-xl before:absolute before:inset-0 before:z-10 before:border before:border-gray-200 before:rounded-xl before:transition before:hover:border-2 before:hover:border-blue-600 before:hover:shadow-lg"
+            href="#">
+            <div class="relative pt-[50%]">
+              <img class="size-full absolute top-0 start-0 object-cover rounded-t-xl"
+                src="https://preline.co/assets/thumbnails/examples/blog-sections.webp" alt="Blog Sections">
+
+            </div>
+            <div class="bg-white py-3.5 px-4 rounded-b-xl  ">
+              <h3 class="text-sm text-gray-900 font-medium  ">Blog Sections <span
+                  class="inline bg-blue-50 border border-blue-300 text-blue-600 text-[.6125rem] leading-4 uppercase align-middle rounded-full py-0.5 px-2 ">+2
+                  new</span></h3>
+              <p class="mt-0.5 text-sm text-gray-400  ">11 components</p>
+            </div>
+          </a>
+         
+          <a class="relative rounded-xl before:absolute before:inset-0 before:z-10 before:border before:border-gray-200 before:rounded-xl before:transition before:hover:border-2 before:hover:border-blue-600 before:hover:shadow-lg "
+            href="#">
+            <div class="relative pt-[50%]">
+              <img class="size-full absolute top-0 start-0 object-cover rounded-t-xl "
+                src="https://preline.co/assets/thumbnails/examples/blog-articles.webp" alt="Blog Articles">
+
+            </div>
+            <div class="bg-white py-3.5 px-4 rounded-b-xl ">
+              <h3 class="text-sm text-gray-900 font-medium ">Blog Articles </h3>
+              <p class="mt-0.5 text-sm text-gray-400 ">2 components</p>
+            </div>
+          </a>
+         
+          <a class="relative rounded-xl before:absolute before:inset-0 before:z-10 before:border before:border-gray-200 before:rounded-xl before:transition before:hover:border-2 before:hover:border-blue-600 before:hover:shadow-lg "
+            href="#">
+            <div class="relative pt-[50%]">
+              <img class="size-full absolute top-0 start-0 object-cover rounded-t-xl  "
+                src="https://preline.co/assets/thumbnails/examples/team-sections.webp" alt="Team Sections">
+
+            </div>
+            <div class="bg-white py-3.5 px-4 rounded-b-xl ">
+              <h3 class="text-sm text-gray-900 font-medium  ">Team Sections </h3>
+              <p class="mt-0.5 text-sm text-gray-400  ">4 components</p>
+            </div>
+          </a>
+        </div>
+      </div>
+    </div>
+
+
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style></style>
